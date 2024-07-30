fa61a1d3888ef1d05e32a1199bd6f1bb7eedbbde
chitti <chittiakhil53@gmail.com>
2024-07-30 11:37:18 +0530

    ApplicationContext

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -1,2 +1,32 @@
-package org.example;public class AppConfig {
+// AppConfig.java
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    private  String state;
+    private String City;
+
+    @Bean
+    public MyBean myBean() {
+        return new MyBean();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
 }
--- src/main/java/org/example/MyBean.java
+++ src/main/java/org/example/MyBean.java
@@ -1,2 +1,10 @@
-package org.example;public class MyBean {
+// MyBean.java
+package org.example;
+
+
+public class MyBean {
+    public void doSomething() {
+        System.out.println("MyBean is doing something!");
+    }
+
 }
